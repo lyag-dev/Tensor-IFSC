8e3a646131d799b61819583ed73416cc1d2bb020
lyag-dev <lyagushkatribola@gmail.com>
2026-03-19 22:05:29 -0300

    testando o pytorch 1.0

--- mistral-test.py
+++ mistral-test.py
@@ -1,9 +1,11 @@
+import torch
+torch.set_num_threads(12)
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1", local_files_only=True)
 
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", local_files_only=True, device_map="auto")
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", local_files_only=True, device_map="cpu")
 
 inputs = tokenizer("Me explique quem é você?, me responda em português", return_tensors="pt")
-outputs = model.generate(**inputs, max_new_tokens=600)
+outputs = model.generate(**inputs, max_new_tokens=100)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
